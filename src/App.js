4c6dcf4ba387e0ef37d09b361c54e651ee8ce2c1
Ramesh <rameshpvr97@gmail.com>
2021-08-16 18:17:47 +0530

    Footer Inclusion

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './App.css';
 import Quote from './Components/Quote';
 
+const Footer = "footer";
+
 function App() {
   return (
     <div>
       <Quote/>
+      <Footer className="footer">
+        <a href="https://github.com/rameshpvr" target="_blank" rel="noopener noreferrer">-By Ramesh <span><i class="fab fa-github fa-2x"></i></span></a>
+      </Footer>
     </div>
   )
 }
